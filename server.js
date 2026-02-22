150ca73e373a859d2ec645eb4f37a0780b8cbd1e
DasunD2002 <dasun5967@gmail.com>
2026-02-22 11:11:24 +0530

    add_css

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
+    res.write(`<h1 style="font-size: 40px; color: brown">Hello world!</h1>`);
     res.end();
 }).listen(8080);
 console.log('Server running at http://localhost:8080/');
